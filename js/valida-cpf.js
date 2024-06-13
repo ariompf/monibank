816002db7725a89026a2728a64823c35f58e3d56
Ariom Pimenta <ariompf@gmail.com>
2024-06-13 07:53:02 -0300

    scripts for form validity: cpf and idade

---
+++ js/valida-cpf.js
@@ -0,0 +1,60 @@
+export default function ehUmCPF(campo) {
+    const cpf = campo.value.replace(/\.|-/g, '');
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        console.log('Esse CPF não existe.')
+    } else {
+        console.log('Existe!');
+    }
+}
+
+function validaNumerosRepetidos(cpf) {
+    const numerosRepetidos = [
+        '0000000000',
+        '1111111111',
+        '2222222222',
+        '3333333333',
+        '4444444444',
+        '5555555555',
+        '6666666666',
+        '7777777777',
+        '8888888888',
+        '9999999999'
+    ]
+    return numerosRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for(let tamanho = 0; tamanho <9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for(let tamanho = 0; tamanho <10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[10];
+}
